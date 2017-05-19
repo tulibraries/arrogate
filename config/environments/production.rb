a2c6b9c06e873afe1d0c07e9e83eda9ffe318664
Steven Ng <steven.ng@temple.edu>
2017-05-19 15:33:48 -0400

    Specify production application directory

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'http://libqa.library.temple.edu/libguide_search'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Deploy to libguide_search subdirectory
+  config.relative_url_root = '/libguide_search'
 end
